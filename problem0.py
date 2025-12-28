ead3c46d1d5b9bfb089a5a88ab2ed7371e479550
EmirTevfik10 <emirtevfik10@gmail.com>
2025-12-28 10:03:12 +0300

    performans iyileştirmeleri yapıldı.

--- problem0.py
+++ problem0.py
@@ -1,20 +1,8 @@
-def tek_mi():
-    sayilar = [i**2 for i in range(1,991001,1)]
-    tek_kare = []
-    for sayi in sayilar:
-        if sayi % 2 != 0:
-            tek_kare.append(sayi)
-    return tek_kare
-
-def topla(tek_kare):
-    toplam = 0
-    for sayi in tek_kare:
-        toplam += sayi
-    return toplam
+def tek_kareleri_üret():
+    return (i*i for i in range(1,991001,2))
 
 def main():
-    tek_kare = tek_mi()
-    toplam = topla(tek_kare)
+    toplam = sum(tek_kareleri_üret())
     print(toplam)
 
 main()
