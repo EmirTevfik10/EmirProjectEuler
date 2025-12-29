852a7b64b90097d80f19dbf80a61251446606b93
EmirTevfik10 <emirtevfik10@gmail.com>
2025-12-29 23:49:57 +0300

    Performans sıkıntısı çözüldü.

--- problem3.py
+++ problem3.py
@@ -1,30 +1,25 @@
-def prime_numbers_generate(max_value):
-    """
-    Generate prime numbers.
-    """
-    prime_numbers = [2]
-    for num in range(3,max_value,2):
-        print(prime_numbers[-1])
-        for i in range(2,num):
-            if num % i == 0:
-                break
-            prime_numbers.append(num)
-    return prime_numbers
-
-def find_largest_prime_factor(max_value,prime_numbers):
+def find_largest_prime_factor(max_value):
     """
     Finds largest prime factor of Max Value.
     """
-    max_prime = 2
-    for prime in prime_numbers:
-        if max_value % prime:
-            max_prime = prime
+    num = 3
+    max_prime = 1
+    while max_value % 2 == 0:
+        max_prime = 2
+        max_value //= 2
+    
+    while num * num <= max_value:
+        while max_value % num == 0:
+            max_prime = num
+            max_value //= num
+        num += 2
+    if max_value > 1:
+        max_prime = max_value
     return max_prime
 
 def main():
     max_value = 600851475143
-    prime_numbers = prime_numbers_generate(max_value)
-    max_prime = find_largest_prime_factor(max_value,prime_numbers)
+    max_prime = find_largest_prime_factor(max_value)
     print(max_prime)
 
 main()
