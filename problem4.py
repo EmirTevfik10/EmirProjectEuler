b55e6616a191ea8e06a289b07548ec933f520570
EmirTevfik10 <emirtevfik10@gmail.com>
2026-01-19 23:02:18 +0300

    Polindrom dizisi silindi. Yerine if kontrolü getirildi.

--- problem4.py
+++ problem4.py
@@ -1,11 +1,11 @@
 def three_digit_number():
-    polindrom = []
+    polindrom = 0
     for i in range(999,99,-1):
         for j in range(i,99,-1):
             sonuc = i * j
-            if str(sonuc) == str(sonuc)[::-1]:
-                polindrom.append(sonuc)
-    return max(polindrom)
+            if str(sonuc) == str(sonuc)[::-1] and sonuc > polindrom: 
+                polindrom = sonuc
+    return polindrom
 def main():
     max_polindrom = three_digit_number()
     print(max_polindrom)
