af5ad4ade96482291b414fd894244f6c4f1eb484
EmirTevfik10 <emirtevfik10@gmail.com>
2026-01-19 22:58:58 +0300

    Çarpım sayısı azaltıldı.

--- problem4.py
+++ problem4.py
@@ -1,7 +1,7 @@
 def three_digit_number():
     polindrom = []
     for i in range(999,99,-1):
-        for j in range(999,99,-1):
+        for j in range(i,99,-1):
             sonuc = i * j
             if str(sonuc) == str(sonuc)[::-1]:
                 polindrom.append(sonuc)
