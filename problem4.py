733a75d51ecc7856717d66d5a9de5da8fd852e3b
EmirTevfik10 <emirtevfik10@gmail.com>
2026-01-19 22:48:08 +0300

    Problem çözüldü.

---
+++ problem4.py
@@ -0,0 +1,12 @@
+def three_digit_number():
+    polindrom = []
+    for i in range(999,99,-1):
+        for j in range(999,99,-1):
+            sonuc = i * j
+            if str(sonuc) == str(sonuc)[::-1]:
+                polindrom.append(sonuc)
+    return max(polindrom)
+def main():
+    max_polindrom = three_digit_number()
+    print(max_polindrom)
+main()
