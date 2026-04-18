ae875ee887fa5ff9d3cbc5cdb6b439511a7b7cae
EmirTevfik10 <emirtevfik10@gmail.com>
2026-04-18 07:08:56 +0300

    Fonksiyonel hale getirildi

--- problem7.py
+++ problem7.py
@@ -1,18 +1,24 @@
-prime_numbs = [2]
-numb = 3
-
-while len(prime_numbs) < 10001:
-    isPrime = True
-    
+def is_prime(numb,prime_numbs):
     for prime in prime_numbs:
         if prime * prime > numb:
             break
         if numb % prime == 0:
-            isPrime = False
-            break
+            return False
 
-    if isPrime:    
-        prime_numbs.append(numb) 
-    
-    numb += 2
-print(prime_numbs[-1])
+    return True
+
+def nt_prime(n):
+    prime_numbs = [2]
+    numb = 3
+
+    while len(prime_numbs) < n:
+        if is_prime(numb, prime_numbs):
+            prime_numbs.append(numb) 
+        numb += 2
+
+    return prime_numbs[-1]
+
+def main():
+    print(nt_prime(10001))
+
+main()
