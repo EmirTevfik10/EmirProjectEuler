04cee4292a7c3531fc52b0bfb543a3dcffae1273
EmirTevfik10 <emirtevfik10@gmail.com>
2026-04-24 03:50:16 +0300

    Performans iyileştirilmesi ve yazım düzeltmeleri yapıldı.

--- problem9.py
+++ problem9.py
@@ -1,28 +1,18 @@
-def pythagoraen_triplet(a,b,c):
-    if (a < b and b < c) and (a**2 + b**2 == c**2):
-        return True
+def is_pythagorean(a,b,c):
+    return a < b and b < c and a**2 + b**2 == c**2
     
-def find_specific_numbs(numb):
-    first_list = []
-    last_list = []
-    a = 1
-    b = 2 
-    c = numb - 3
-    while c > b:
-        first_list.append([a,b,c])
-        b += 1
-        c -= 1
-    for a,b,c in first_list:
-        while b > a and c > b:
-            last_list.append([a,b,c])
-            a += 1
-            c -= 1
-    for a,b,c in last_list:
-        if pythagoraen_triplet(a,b,c):
-            return a,b,c
+def find_triplet(total):
+    for a in range(1,total):
+        for b in range(a + 1, total):
+            c = total - a - b
+            if c <= b:
+                break
+            if is_pythagorean(a, b, c):
+                return a, b, c 
+
 def main():
-    a,b,c = find_specific_numbs(1000)
-    print(a * b * c)
+    a, b, c = find_triplet(1000)
+    print(f"{a} x {b} x {c} = {a * b * c}")
 
 main()
         
